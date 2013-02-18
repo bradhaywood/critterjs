2918b71c31f0a8fb118cc4394c30f4001bb41f00
Brad <brad@geeksware.(none)>
2013-02-18 20:45:24 +0000

    Added testing. Hooray

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+function ok(str) {
+    document.writeln('<font color="green">' + str + '</font><br />');
+}
+
+function not_ok(str) {
+    document.writeln('<font color="red">' + str + '</font><br />');
+}
